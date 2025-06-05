d2dcdb5cdf22717b7ad82ff10e6183a11ec675af
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-06-05 05:57:42 +0300

    Register and login Finito

--- src/app/(pages)/(auth)/login/page.js
+++ src/app/(pages)/(auth)/login/page.js
@@ -1,11 +1,125 @@
-import React from 'react'
+"use client";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button } from "@/components/ui/button";
+import {
+  Card, CardContent, CardFooter, CardHeader, CardTitle
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import axios from 'axios';
+import Link from "next/link";
+// Yup Validation Schema
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+  .matches(
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    "Invalid email format"
+  )
+  .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 const page = () => {
+  const handleLogin = (values) => {
+    console.log("Login Data:", values);
+    // API call goes here
+    axios.post('http://localhost:8000/api/login',values)
+               .then( (res) => {
+                const data = res.data.data;
+                const token = res.data.token
+                console.log(data, token);
+                const user = {
+                  id: data.id,
+                  name: data.name,
+                  email: data.email,
+                  role: data.role
+                }
+                localStorage.setItem("user", JSON.stringify(user));
+                localStorage.setItem("token", token);
+                console.log(user)
+                console.log(token)
+               })
+  };
+
   return (
-    <div>
-      login page
-    </div>
-  )
-}
+    <>
+      <header className="bg-[#ffcc41] text-black font-medium p-4 h-60 m-6 mb-0 rounded-lg flex items-center">
+        <h1 className="text-6xl ml-40 ">User Dashboard</h1>
+      </header>
+      <div className="min-h-screen flex items-center justify-center">
+        <Card className="w-full max-w-sm border-1 rounded-1xl">
+          <CardHeader>
+            <CardTitle className="text-center text-3xl">Login</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={LoginSchema}
+              onSubmit={(values, actions) => {
+                handleLogin(values);
+                actions.setSubmitting(false);
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form className="flex flex-col gap-6">
+                  {/* Email */}
+                  <div className="grid gap-2">
+                    
+                    <Field
+                      name="email"
+                      type="text"
+                      as={Input}
+                      placeholder="Email*"
+                      className="border rounded-none p-6 text-5xl"
+                      title="Please fill out this field"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+
+                  {/* Password */}
+                  <div className="grid gap-2">
+                    
+                    <Field
+                      name="password"
+                      type="password"
+                      as={Input}
+                      placeholder="Password*"
+                      className="border rounded-none p-6 text-5xl"
+                      title="Please fill out this field"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+
+                  <Button type="submit" disabled={isSubmitting} className="bg-[#ffcc41] text-black text-1xl p-6 rounded-none hover:bg-amber-400">
+                    {isSubmitting ? "Logging in..." : "Login"}
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <div className="text-center">
+              <span className="text-muted-foreground">Not a member?</span>
+              <Link href="/register" passHref  className="text-black m-2 hover:underline" >
+                Register here
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
+};
 
 export default page
--- src/app/(pages)/(auth)/register/page.js
+++ src/app/(pages)/(auth)/register/page.js
@@ -1,11 +1,222 @@
-import React from 'react'
+"use client";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button } from "@/components/ui/button";
+import {
+  Card, CardContent, CardFooter, CardHeader, CardTitle
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import axios from 'axios';
+import Link from "next/link";
+// Yup Validation Schema
+const RegisterSchema = Yup.object().shape({
+  name: Yup.string()
+  .matches(
+    /^[A-Za-zÀ-ÿ' -]{3,70}$/, 
+    "Invalid name! Name must be 3–70 characters and only include letters, spaces, hyphens, or apostrophes."
+  )
+  .required("Name is required")
+  ,
+  email: Yup.string()
+  .matches(
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    "Invalid email format! It must be like m@example.com"
+  )
+  .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  password_confirmation: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required("Password confirmation is required"),
+  role: Yup.string()
+    .required("Role is required"),
+  verification_document: Yup.mixed()
+    .required("Verification document is required")
+    .test("fileSize", "File size is too large", value => {
+      return value && value.size <= 5242880; // 5MB limit
+    })
+    .test("fileType", "Unsupported file format", value => {
+      return value && ["image/jpeg", "image/png", "application/pdf"].includes(value.type);
+    }),
+});
 
 const page = () => {
+  const handleRegister = (values) => {
+    console.log("Register Data:", values);
+    // API call goes here
+    const formData = new FormData();
+    formData.append("name", values.name);
+    formData.append("email", values.email);
+    formData.append("password", values.password);
+    formData.append("password_confirmation", values.password_confirmation);
+    formData.append("role", values.role);
+    formData.append("verification_document", values.verification_document);
+
+    axios.post('http://localhost:8000/api/register', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    }).then( (res) => {
+        const data = res.data.data;
+        const token = res.data.token
+        console.log(data, token);
+        const user = {
+          id: data.id,
+          name: data.name,
+          email: data.email,
+          role: data.role
+        }
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("token", token);
+        console.log(user)
+        console.log(token)
+        })
+  };
+
   return (
-    <div>
-      register page
-    </div>
-  )
-}
+    <>
+      <header className="bg-[#ffcc41] text-black font-medium p-4 h-60 m-6 mb-0.5 rounded-lg flex items-center">
+        <h1 className="text-6xl ml-40 ">User Dashboard</h1>
+      </header>
+      <div className="min-h-screen flex items-center justify-center">
+        <Card className="w-full max-w-sm border rounded-lg">
+          <CardHeader>
+            <CardTitle className="text-center text-3xl">Register</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Formik
+              initialValues={{  name: "", email: "", password: "", password_confirmation: "", role: "student", verification_document: "", }}
+              validationSchema={RegisterSchema}
+              onSubmit={(values, actions) => {
+                handleRegister(values);
+                actions.setSubmitting(false);
+              }}
+            >
+              {({ isSubmitting, setFieldValue }) => (
+                <Form className="flex flex-col gap-6" encType="multipart/form-data">
+                  {/* Name */}
+                  <div className="grid gap-2">
+                    
+                    <Field
+                      name="name"
+                      type="text"
+                      as={Input}
+                      placeholder="Name*"
+                      className="border rounded-none p-6 text-muted-foreground"
+                      title="Please fill out this field"
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+                  {/* Email */}
+                  <div className="grid gap-2">
+                    
+                    <Field
+                      name="email"
+                      type="text"
+                      as={Input}
+                      placeholder="Email*"
+                      className="border rounded-none p-6 text-muted-foreground"
+                      title="Please fill out this field"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+
+                  {/* Password */}
+                  <div className="grid gap-2">
+                    
+                    <Field
+                      name="password"
+                      type="password"
+                      as={Input}
+                      placeholder="Password*"
+                      className="border rounded-none p-6 text-muted-foreground"
+                      title="Please fill out this field"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+                  {/* Password_confirmation */}
+                  <div className="grid gap-2">
+                    
+                    <Field
+                      name="password_confirmation"
+                      type="password"
+                      as={Input}
+                      placeholder="Confirm Password*"
+                      className="border rounded-none p-6 text-muted-foreground"
+                      title="Please fill out this field"
+                    />
+                    <ErrorMessage
+                      name="password_confirmation"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+                  {/* Role */}
+                  <div className="grid gap-2">
+                    <Field
+                      name="role"
+                      as="select"
+                      className="border rounded-none p-4 text-1xl text-muted-foreground"
+                      title="Please select a role"
+                    >
+                      <option value="student" label="Student" />
+                      <option value="owner" label="Owner" />
+                    </Field>
+                    <ErrorMessage
+                      name="role"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+                  {/* Verification Document */}
+                  <div className="grid gap-2">
+                    <input
+                      name="verification_document"
+                      type="file"
+                      onChange={(event) => {
+                        setFieldValue("verification_document", event.currentTarget.files[0]);
+                      }}
+                      className="border rounded-none text-muted-foreground p-4"
+                    />
+                    <ErrorMessage
+                      name="verification_document"
+                      component="div"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+                  {/* Submit Button */}
+                  <Button type="submit" disabled={isSubmitting} className="bg-[#ffcc41] text-black text-1xl p-6 rounded-none hover:bg-amber-400">
+                    {isSubmitting ? "Redirecting..." : "Register"}
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <div className="text-center">
+              <span className="text-muted-foreground">Have an account?</span>
+              <Link href="/login" passHref  className="text-black m-2 hover:underline" >
+                Log in
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
+};
 
 export default page
