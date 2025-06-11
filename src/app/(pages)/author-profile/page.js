bf8f25d9f13eccfba918703e662f314c6b6f3938
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-06-11 23:13:35 +0300

    add profile page for owner

---
+++ src/app/(pages)/author-profile/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import banner from '@/assets/images/banner.jpg'
+import Image from 'next/image'
+const page = () => {
+  return (
+   <>
+    
+   </>
+  )
+}
+
+export default page
+
+
+
+
+
